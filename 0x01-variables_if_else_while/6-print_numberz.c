e8a26ba17fdf687998eb7dfa3b156e1a22a3eda0
tahanijabir <tahani.jabir93@gmail.com>
2022-06-03 15:15:25 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers since 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
