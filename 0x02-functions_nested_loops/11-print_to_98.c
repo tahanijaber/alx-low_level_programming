8f9b276d3cc567e06b0a9dee149c9bc080921213
tahanijabir <tahani.jabir93@gmail.com>
2022-06-08 06:22:16 -0700

    98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - a function that prints all numbers from n to 98
- * in both cses when @n is > or < 98
- * @n: the starting number
- * Return: Always 0
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * user input's number prints to 98, regardless < 98 or > 98
+ * @n: number input
+ * Return: Always 0 (Success)
  */
+
 void print_to_98(int n)
 {
 	if (n < 98)
@@ -26,14 +27,15 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
 		}
-		printf(", ");
 	}
-	n--;
-}
-else
-{
-	printf("98");
-}
-printf("\n");
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
 }
