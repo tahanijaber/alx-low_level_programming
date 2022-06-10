72cc68b19f6d456708f28ae2bd5d334b71f8f23b
tahanijabir <tahani.jabir93@gmail.com>
2022-06-10 12:51:36 -0700

    The problem of distinguishing

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,27 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
- * main - prints prime factors of 612852475143
- *
- * Return: Always 0 (Success)
+ * main - program that prints out the highest prime
+ * Return: 0
  */
 int main(void)
 {
+	int i = 2;
+	long n = 612852475143;
+
+	while (i < n)
+	{
+		while (n % i == 0)
+		{
+			if (n == i)
+			{
+				break;
+			}
+			n /= i;
+		}
+		i++;
+	}
+	printf("%lu\n", n);
 	return (0);
 }
