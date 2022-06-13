08342eab64ea3f6142f4288b97cd8438e1282f9b
tahanijabir <tahani.jabir93@gmail.com>
2022-06-13 05:53:27 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * char *_strcpy - copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *d = dest;
+
+	for (; *src != '\0'; src++)
+	{
+		*dest = *src;
+		dest++;
+	}
+
+	*dest = *src;
+
+	return (d);
+}
+
