d735df3c183c80d77f87087a5001a281290fc003
tahanijabir <tahani.jabir93@gmail.com>
2022-06-15 08:53:05 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - Copy a string starting from index 0 of dest
+ * @dest: string
+ * @src: string
+ * @n: number of chars to copy over
+ * Return: `dest`
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
