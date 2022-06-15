6e943878a69f41efff9be36d22a0d53694d53054
tahanijabir <tahani.jabir93@gmail.com>
2022-06-15 09:19:15 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - changeing letters of array from lowercase to uppercase
+ * @s: array string
+ * Return: uppercase string output
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 96 && s[i] <= 123)
+			s[i] -= 32;
+	}
+	return (s);
+}
